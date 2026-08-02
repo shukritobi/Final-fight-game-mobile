8fa12eca60d92c62f27a64a27b911a56d42ef2a8
shukry sharby <shukrysharby91@gmail.com>
2026-08-02 11:58:26 +0800

    Remove service worker and purge stale audio caches

--- render.js
+++ render.js
@@ -141,4 +141,12 @@ document.addEventListener('dblclick', event => event.preventDefault(), { passive
 addEventListener('contextmenu', event => event.preventDefault());
 
 requestAnimationFrame(loop);
-if ('serviceWorker' in navigator) navigator.serviceWorker.register('./sw.js').catch(() => {});
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations()
+    .then(registrations => Promise.all(registrations.map(registration => registration.unregister())))
+    .catch(() => {});
+}
+if ('caches' in window) {
+  caches.keys().then(keys => Promise.all(keys.map(key => caches.delete(key)))).catch(() => {});
+}
